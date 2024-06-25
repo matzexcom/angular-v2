df91586da8df791cc7889cfecc1d67357b9f66b3
Matthias Vogel <hub@birdsview.dev>
2024-06-25 11:15:06 +0200

    Essentials - add task

---
+++ src/app/tasks/add-task/add-task.component.ts
@@ -0,0 +1,32 @@
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
+
+@Component({
+  selector: 'app-add-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css',
+})
+export class AddTaskComponent {
+  onClosed = output<void>();
+  newTaskSubmitted = output<NewTaskData>();
+  title = signal('');
+  summary = signal('');
+  date = signal('');
+
+  public onClose() {
+    this.onClosed.emit();
+  }
+
+  onSubmit() {
+    this.newTaskSubmitted.emit({
+      title: this.title(),
+      summary: this.summary(),
+      dueDate: this.date(),
+    });
+
+    this.onClosed.emit();
+  }
+}
