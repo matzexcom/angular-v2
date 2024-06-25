679855bc5e608e960164aeca8633fbad8a78f9a0
Matthias Vogel <hub@birdsview.dev>
2024-06-25 20:05:51 +0200

    scaffolding

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ServerStatusComponent } from '../server-status/server-status.component';
+import { TrafficComponent } from '../traffic/traffic.component';
+import { TicketsComponent } from '../tickets/tickets.component';
+import { DashboardItemComponent } from './dashboard-item/dashboard-item.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+    DashboardItemComponent,
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {}
