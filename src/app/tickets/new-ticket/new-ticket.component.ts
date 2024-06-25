291fecbf4a4de1e96bf64ba06c89b14849c2778b
Matthias Vogel <hub@birdsview.dev>
2024-06-25 21:41:24 +0200

    new ticket onSubmit

--- src/app/tickets/new-ticket/new-ticket.component.ts
+++ src/app/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../ui/button/button.component';
 import { ControlComponent } from '../../ui/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  title = signal<string>('');
+  request = signal<string>('');
+  form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  onSubmit() {
+    console.log(this.title(), this.request());
+    this.form().nativeElement.reset();
+  }
+}
