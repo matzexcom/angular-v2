da5bfa9f0df6e3c720f2b088e33e8af9c1d8fc35
Matthias Vogel <hub@birdsview.dev>
2024-06-25 20:37:05 +0200

    dynamic classes

--- src/app/server-status/server-status.component.ts
+++ src/app/server-status/server-status.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type ServerStatus = 'online' | 'offline' | 'unknown';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: ServerStatus = 'online';
 
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.floor(Math.random() * 3);
+      switch (rnd) {
+        case 0:
+          this.currentStatus = 'online';
+          break;
+        case 1:
+          this.currentStatus = 'offline';
+          break;
+        case 2:
+          this.currentStatus = 'unknown';
+          break;
+        default:
+          this.currentStatus = 'online';
+      }
+    }, 1500);
+  }
 }
