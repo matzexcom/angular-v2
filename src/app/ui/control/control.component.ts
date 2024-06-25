e1a5025d7edef4608faa1e0e54e17914fb86246a
Matthias Vogel <hub@birdsview.dev>
2024-06-25 20:04:43 +0200

    ui components

---
+++ src/app/ui/control/control.component.ts
@@ -0,0 +1,39 @@
+import { Component, input } from '@angular/core';
+
+type InputType =
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week'
+  | 'textarea';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+})
+export class ControlComponent {
+  label = input<string>('');
+  type = input<InputType>('text');
+  id = input.required<string>();
+}
